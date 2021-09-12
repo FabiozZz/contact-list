e2092c5b4dea69f001fcf483d8ede3b41d8114ed
fabiozzz <mr.fabiozzz@gmail.com>
2021-09-13 00:48:58 +0300

    initial app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+      <post-form @create="createPost"/>
+    <post-list 
+    :posts="posts"/>
   </div>
-  <router-view/>
 </template>
-
+<script>
+import PostForm from '@/components/PostForm'
+import PostList from '@/components/PostList'
+export default {
+    components:{
+        PostList,PostForm
+    },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "Пост о JavaScript",
+          description: "JavaScript - универсальный язык программирования",
+        },
+        {
+          id: 2,
+          title: "Пост о HTML",
+          description: "HTML - единственно доступный язык разметки",
+        },
+        {
+          id: 3,
+          title: "Пост о CSS",
+          description:
+            "CSS - язык форматирования, оформления внешнего вида сайта",
+        },
+        {
+          id: 4,
+          title: "Пост о PHP",
+          description: "PHP - серверный язык программирования",
+        },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+        this.posts.push(post)
+    },
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      class="input"
+      type="text"
+      placeholder="название поста"
+    />
+    <input
+      v-model="post.description"
+      class="input"
+      type="text"
+      placeholder="описание поста"
+    />
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        description: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create',this.post)
+      this.post = {
+      title: "",
+      description: ""
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  margin-top: 15px;
+  padding: 10px 15px;
+  border: 1px solid teal;
+  background: none;
+  align-self: flex-end;
+}
+</style>>
